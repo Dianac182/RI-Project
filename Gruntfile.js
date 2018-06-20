39675f89bbc0cfb3d0b8d6363dfc35f28f7df8e7
Unknown <dianac182@gmail.com>
2018-06-20 20:13:36 +0200

    add crop for mobile images and alt attribute

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,93 +8,100 @@
 
 module.exports = function(grunt) {
 
-    grunt.initConfig({
+	grunt.initConfig({
 
-        responsive_images: {
-            dev: {
-                options: {
-                    engine: 'im',
-                    sizes: [{
-                        /*
-                        Change these:
+		responsive_images: {
+			dev: {
+				options: {
+					engine: 'im',
+					sizes: [{
+						name: "small_cropped",
+						width: '50%',
+						height: '50%',
+						aspectRatio: false,
+					},{	
+						name: "small",
+						width: '500',
+						quality: 30
+					},{
+						name: "medium_cropped",
+						width: '65%',
+						height: '65%',
+						aspectRatio: false,
+					},{
+						name: "medium",
+						width: 600,
+						quality: 30
+					},{
+						name: "large",
+						width: 800,
+						suffix: "_1x",
+						quality: 40
+					},{
+						name: "large",
+						width: 1600,
+						suffix: "_2x",
+						quality: 40
+					}]
+				},
 
-                        width: ,
-                        suffix: ,
-                        quality:
-                        */
-                        name: 'small',
-                        width: 320,
-                        quality: 20
-                    },{
-                        name: 'medium',
-                        width: 640,
-                        quality: 30
-                    },{
-                        name: "large",
-                        width: 1024,
-                        quality: 40
-                    }]
-                },
+				/*
+				You don't need to change this part if you don't change
+				the directory structure.
+				*/
+				files: [{
+					expand: true,
+					src: ['*.{gif,jpg,png}'],
+					cwd: 'images_src/',
+					dest: 'images/'
+				}]
+			}
+		},
 
-                /*
-                You don't need to change this part if you don't change
-                the directory structure.
-                */
-                files: [{
-                    expand: true,
-                    src: ['*.{gif,jpg,png}'],
-                    cwd: 'images_src/',
-                    dest: 'images/'
-                }]
-            }
-        },
+		/* Clear out the images directory if it exists */
+		clean: {
+			dev: {
+				src: ['images'],
+			},
+		},
 
-        /* Clear out the images directory if it exists */
-        clean: {
-            dev: {
-                src: ['images'],
-            },
-        },
+		/* Generate the images directory if it is missing */
+		mkdir: {
+			dev: {
+				options: {
+					create: ['images']
+				},
+			},
+		},
 
-        /* Generate the images directory if it is missing */
-        mkdir: {
-            dev: {
-                options: {
-                    create: ['images']
-                },
-            },
-        },
+		/* Copy the "fixed" images that don't go through processing into the images/directory */
+		copy: {
+			dev: {
+				files: [{
+					expand: true,
+					src: 'images_src/fixed/*.{gif,jpg,png}',
+					dest: 'images/'
+				}]
+			},
+		},
+	
+		/* Configure the official image minification plug-in */
+		imagemin: {
+			dynamic: {
+				files: [{
+					expand: true,
+					cwd: 'images/',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'images/build/'
+				}]
+			}
+		},
+	});
 
-        /* Copy the "fixed" images that don't go through processing into the images/directory */
-        copy: {
-            dev: {
-                files: [{
-                    expand: true,
-                    src: 'images_src/fixed/*.{gif,jpg,png}',
-                    dest: 'images/'
-                }]
-            },
-        },
-
-        /* Configure the official image minification plug-in */
-        imagemin: {
-            dynamic: {
-                files: [{
-                    expand: true,
-                    cwd: 'images/',
-                    src: ['**/*.{png,jpg,gif}'],
-                    dest: 'images/build/'
-                }]
-            }
-        },
-    });
-
-    /*grunt.loadNpmTasks('grunt-run');*/
-    grunt.loadNpmTasks('grunt-responsive-images');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-mkdir');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images', 'imagemin']);
+	grunt.loadNpmTasks('grunt-responsive-images');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-mkdir');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images', 'imagemin']);
 };
-
